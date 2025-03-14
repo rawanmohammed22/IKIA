3cdafcb8f5e31dedc7c867d705f9e3e451d0e4d5
rawanmohammed22 <rawanmohammed22>
2025-03-14 15:33:59 +0200

    fifth

--- IKIA.DAL/Prsistance/Data/ApplicationDbContext.cs
+++ IKIA.DAL/Prsistance/Data/ApplicationDbContext.cs
@@ -17,7 +17,7 @@ namespace IKIA.DAL.Prsistance.Data
     {
         #region       DbSet
 
-        public DbSet<Department> Departmentssss { get; set; }
+        public DbSet<Department> Departmen { get; set; }
 
 
 
